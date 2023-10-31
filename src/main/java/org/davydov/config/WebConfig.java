28d83135ac63d476faf864e7b14966f8db8982d8
Pavel Davydov <davydovpavel@mail.ru>
2023-10-31 20:27:56 +0500

    attempted to configure ui swagger

--- src/main/java/org/davydov/config/WebConfig.java
+++ src/main/java/org/davydov/config/WebConfig.java
@@ -1,18 +1,29 @@
 package org.davydov.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
 
 import java.util.List;
 
 @EnableWebMvc
 @Configuration
+@EnableSwagger2
 @PropertySource("classpath:application.yml")
 @EnableAspectJAutoProxy
 public class WebConfig implements WebMvcConfigurer {
@@ -23,13 +34,30 @@ public class WebConfig implements WebMvcConfigurer {
         converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
     }
 
-//    private ApiInfo getApiInfo(){
-//        return new ApiInfo("book store", "sample desk", "v1", "", "co", "", "");
-//    }
-//
-//    @Bean
-//    public Docket swaggerCommonConfiguration(){
-//        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName())).paths(PathSelectors.any()).build().apiInfo(getApiInfo());
-//    }
+    @Bean
+    public Swagger2DocumentationConfiguration getSwagger2(){
+        return new Swagger2DocumentationConfiguration();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.davydov.in.controller"))
+                .paths(PathSelectors.any())
+                .build()
+        .apiInfo(getApiInfo());
+    }
+    private ApiInfo getApiInfo() {
+        return new ApiInfo("Swagger2 Api Documentation",
+                "How to generate Swagger documentation for your Rest API",
+                "1.0", "urn:tos",
+                new Contact("Pavel Davydov", "www.davydovpavel.org", "davydovpavel@mail.com"),
+                "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+    }
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
 
