473fa87e4506ba36a69d814958279965e0206a6d
Pavel Davydov <davydovpavel@mail.ru>
2023-11-04 15:55:08 +0500

    add springboot app and doc api

---
+++ src/main/java/org/davydov/TrainingYlabBootApplication.java
@@ -0,0 +1,14 @@
+package org.davydov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingYlabBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TrainingYlabBootApplication.class, args);
+    }
+
+}
